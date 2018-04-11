a4a3b29deeccd0548bc872cbfcebb4946092cf15
omelendrez <omar.melendrez@gmail.com>
2018-04-10 23:23:27 -0300

    Draw backbone

---
+++ src/components/Products.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="products">
+    <h1>Productos</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'products',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.products {
+  background-color: aquamarine;
+}
+</style>
